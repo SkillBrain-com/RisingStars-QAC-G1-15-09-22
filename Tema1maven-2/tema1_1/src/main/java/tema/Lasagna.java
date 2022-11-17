b7275070a46a92e7400b3fb7849b49202b2e4b78
anacasu <borceaana95@gmail.com>
2022-11-17 21:18:14 +0200

    tema capitol 2 ex 1

---
+++ Tema1maven-2/tema1_1/src/main/java/tema/Lasagna.java
@@ -0,0 +1,23 @@
+
+public class Lasagna {
+
+  public static int expectedMinutesInOven() {
+    int expectedMinutes = 40;
+    return expectedMinutes;
+  }
+
+  public static int remainingMinutesInOven(int minuteInCuptor) {
+    int remainingTimeInMinutes = expectedMinutesInOven() - minuteInCuptor;
+    return remainingTimeInMinutes;
+  }
+
+  public static int preparationTimeInMinutes(int layers) {
+    int preparationTime = layers * 2;
+    return preparationTime;
+  }
+
+  public static int totalTimeInMinutes(int minute, int layers) {
+    int totalMinutes = preparationTimeInMinutes(layers) + minute;
+    return totalMinutes;
+  }
+}
