fb0aba22350218534d434cbbbc2e18865adc1057
Fsorin <sorinfoca@yahoo.com>
2022-12-07 21:52:41 +0200

    Revert "Tema Selenium + Tema Selenium Demo"
    This reverts commit d65d5149fffa6426b3b96accb8708f66f2bdb7f0.

--- fsorin/TemaSelenium/src/test/java/WebManager.java
+++
@@ -1,9 +0,0 @@
-import io.github.bonigarcia.wdm.WebDriverManager;
-import org.openqa.selenium.chrome.ChromeDriver;
-
-public class WebManager {
-    public static ChromeDriver createDriver() {
-        WebDriverManager.chromedriver().setup();
-        return new ChromeDriver();
-    }
-}
